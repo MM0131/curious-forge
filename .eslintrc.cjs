3a42446017b5e3244519bd4658cc12b1b348a9c8
MM031 <6631503045@lamduan.mfu.ac.th>
2025-11-10 03:12:08 +0700

    chore: eslint - recognize .vue files and relax test ts-comment rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,8 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
+    extraFileExtensions: ['.vue']
   },
   extends: [
     'plugin:vue/vue3-recommended',
@@ -22,4 +23,13 @@ module.exports = {
     ,
     '@typescript-eslint/no-explicit-any': 'off'
   }
+  ,
+  overrides: [
+    {
+      files: ['tests/**/*.ts', 'tests/**/*.js'],
+      rules: {
+        '@typescript-eslint/ban-ts-comment': 'off'
+      }
+    }
+  ]
 }
